9643369ed29cf4777fd2a170e12e2cee4267e965
이민규 <67575226+suslmk-lee@users.noreply.github.com>
2024-06-19 17:42:03 +0900

    update

---
+++ execute.sh
@@ -0,0 +1,3 @@
+docker build -t 44ce789b-kr1-registry.container.nhncloud.com/container-platform-registry/rm-server-slack .
+
+docker push 44ce789b-kr1-registry.container.nhncloud.com/container-platform-registry/rm-server-slack
